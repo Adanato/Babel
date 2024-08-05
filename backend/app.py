066843a30e908beaa6e6994964ca00fd8e40e220
Adam Nguyen <AdanatoNw@gmail.com>
2024-08-05 17:05:50 -0500

    Fixed api

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,10 @@
 from flask import Flask, jsonify, request
-import openai
-from dotenv import load_dotenv
-import os
 
-# Load environment variables from .env file
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 #Custom code imports
 from generation import get_location_generation
 app = Flask(__name__)
-
-
 
 @app.route('/api/generate', methods=['GET'])
 def get_destinations():
@@ -27,7 +20,6 @@ def get_destinations():
     
     # Return the suggestions as a JSON response
     return jsonify(destinations)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/generation.py
+++ backend/generation.py
@@ -1,12 +1,26 @@
-def get_location_generation(prompt, n=3):
-    response = openai.Completion.create(
-        engine="gpt-4o",
-        prompt=prompt,
-        max_tokens=50,
-        n=n,
-        stop=None,
-        temperature=0.7
-    )
+from dotenv import load_dotenv
+import os
 
-    suggestions = [choice['text'].strip() for choice in response.choices]
-    return suggestions
+# Load environment variables from .env file
+load_dotenv()
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+def get_location_generation(prompt):
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ],
+    model="gpt-4-turbo",
+)
+
+    
+    return chat_completion
+
